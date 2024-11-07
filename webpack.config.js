9096241469d82207298b71d56087f6d6498c0fa5
Aksars <artas2014@gmail.com>
2024-11-08 02:42:25 +0300

    Рабочий старт проекта на bootstrap 5 + webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,21 @@
-// 'use strict'
-
-// const path = require('path')
-// const autoprefixer = require('autoprefixer')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/js/main.js',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist')
-//   },
-//   devServer: {
-//     static: path.resolve(__dirname, 'dist'),
-//     port: 8080,
-//     hot: true, 
-//     watchFiles: ["src/*.html"],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({ template: './src/index.html' })
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(scss)$/,
-//         use: [
-//           {
-//             // Adds CSS to the DOM by injecting a `<style>` tag
-//             loader: 'style-loader'
-//           },
-//           {
-//             // Interprets `@import` and `url()` like `import/require()` and will resolve them
-//             loader: 'css-loader'
-//           },
-//           {
-//             // Loader for webpack to process CSS with PostCSS
-//             loader: 'postcss-loader',
-//             options: {
-//               postcssOptions: {
-//                 plugins: [
-//                   autoprefixer
-//                 ]
-//               }
-//             }
-//           },
-//           {
-//             // Loads a SASS/SCSS file and compiles it to CSS
-//             loader: 'sass-loader'
-//           }
-//         ]
-//       }
-//     ]
-//   }
-// }
-
 const path = require('path')
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true,
+        watchFiles: ["src/*.html"],       
+    },
+    performance: {        
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
     entry: [
         __dirname + '/src/js/main.js',
-        __dirname + '/src/scss/styles.scss'
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -77,17 +31,29 @@ module.exports = {
                 test: /\.scss$/,
                 exclude: /node_modules/,
                 use: [
-                    'sass-loader'
+                    { loader: MiniCssExtractPlugin.loader },
+                    'css-loader',
+                    'sass-loader',
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    },
                 ],
-                type: 'asset/resource',
-                generator: {
-                    filename: 'css/[name].min.css'
-                },
+
             }
         ]
     },
     plugins: [
-        new RemoveEmptyScriptsPlugin(),  
-        new HtmlWebpackPlugin({ template: './src/index.html' })      
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].min.css',
+        }),
+        new HtmlWebpackPlugin({ template: './src/index.html' })
     ],
 };
